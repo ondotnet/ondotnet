5dd1bce8c9f8272f262a092375464051304322bd
kushal <kushaldeveloper@gmail.com>
2020-02-09 20:11:29 -0700

    generate component and service for weather
    [kushal@ideapadflex-kushal angularfrontend]$ ng generate service weather
    CREATE src/app/weather.service.spec.ts (362 bytes)
    CREATE src/app/weather.service.ts (136 bytes)
    [kushal@ideapadflex-kushal angularfrontend]$ ng generate component weather
    CREATE src/app/weather/weather.component.sass (0 bytes)
    CREATE src/app/weather/weather.component.html (22 bytes)
    CREATE src/app/weather/weather.component.spec.ts (635 bytes)
    CREATE src/app/weather/weather.component.ts (280 bytes)
    UPDATE src/app/app.module.ts (479 bytes)
    [kushal@ideapadflex-kushal angularfrontend]$ ^C
    [kushal@ideapadflex-kushal angularfrontend]$

---
+++ angularfrontend/src/app/weather/weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.sass']
+})
+export class WeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
