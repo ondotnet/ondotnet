8206aa52fa33ba397b4e32654f69e31452142f07
kushal <kushaldeveloper@gmail.com>
2020-02-29 22:00:14 -0700

    set title of document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'ondotnet-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ondotnetangularfrontend';
+  constructor(private titleService:Title) {
+    this.titleService.setTitle('on dotNET');
+  }
 }
